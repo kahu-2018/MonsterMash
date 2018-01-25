61e986167145ea27d13248378e4c9205949640a8
Melissa Mepham <m.a.mepham@gmail.com>
2018-01-26 12:19:25 +1300

    Added some good changes and some to be edited

--- routes.js
+++ routes.js
@@ -26,14 +26,30 @@ router.get('/profiles/:id', (req, res) => {
   db('monsters')
     .select()
     .join('cities', 'monsters.id', '=', 'cities.monster_id')
-    .where('monsters.id', 'id')
+    .where('monsters.id', id)
     .first()
     .then((monster) => {
-      // monster = {monster:"hello", img:"monster-moo.png", description:"scary"}
       res.render('profiles', monster)
     })
     .catch((err) => {
       res.send("Monsters have taken control of the server")
+    })
+})
+
+router.get('/cities/:id', (req, res) => {
+  var db = req.app.get('db')
+  var id = req.params.id
+  db('cities')
+    .select()
+    .join('monsters')
+    .where('cities.id', id)
+    .first()
+    .then((city) => {
+      //city['destroyed']
+      res.render('city', city)
+    })
+    .catch((err) => {
+      res.send("Cities have taken control of the monsters")
     })
 })
 
