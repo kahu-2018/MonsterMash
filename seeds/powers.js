76a016557358936b4a2e6815eea497e64ccc7c54
kahu2018 <fake@fake.com>
2018-01-26 10:37:28 +1300

    Created powers seed

--- seeds/powers.js
+++ seeds/powers.js
@@ -1,13 +1,20 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('powers').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('powers').insert([
+        {id: 1, power: 'Absorb', monster_id: 1, city_id:},
+        {id: 2, power: 'Desolve', monster_id: 1, city_id:},
+        {id: 3, power: 'Plant Power', monster_id: 2, city_id:},
+        {id: 4, power: 'Bush Fire', monster_id: 2, city_id:},
+        {id: 5, power: 'Tail Swipe', monster_id: 3, city_id:},
+        {id: 6, power: 'Stomp', monster_id: 3, city_id:},
+        {id: 7, power: 'Eat', monster_id: 4, city_id:},
+        {id: 8, power: 'Smash', monster_id: 4, city_id:},
+        {id: 9, power: 'Hurricane Breath', monster_id: 5, city_id:},
+        {id: 10, power: 'Glass-shattering Song', monster_id: 5, city_id:}
       ]);
     });
 };
