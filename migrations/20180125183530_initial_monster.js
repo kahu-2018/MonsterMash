53915ea034473ec95e269d01cd57dba100646964
kahu2018 <fake@fake.com>
2018-01-26 10:08:11 +1300

    Created monsters table

--- migrations/20180125183530_initial_monster.js
+++ migrations/20180125183530_initial_monster.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists
+  return knex.schema.createTableIfNotExists('monsters', function (table) {
+    table.increments()
+    table.string('monster')
+    table.string('monster_img')
+    table.string('description')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('monsters')
 };
