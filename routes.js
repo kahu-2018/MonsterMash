fda1d58e77219022c034485beba6de0d9a6f0dd4
Melissa Mepham <m.a.mepham@gmail.com>
2018-01-26 10:25:51 +1300

    profiles routes

--- routes.js
+++ routes.js
@@ -17,4 +17,22 @@ router.get('/', function (req, res) {
     .catch(err => res.send("RAWR, an error!", err))
 })
 
+
+router.get('/profiles/:id', (req, res) => {
+  var db = req.app.get('db')
+  var id = req.params.id
+  db('monsters')
+    .select()
+    .join('cities', 'monsters.id', '=', 'cities.monster_id')
+    .where('monsters.id', 'id')
+    .first()
+    .then((monster) => {
+      // monster = {monster:"hello", img:"monster-moo.png", description:"scary"}
+      res.render('profiles', monster)
+    })
+    .catch((err) => {
+      res.send("Monsters have taken control of the server")
+    })
+})
+
 module.exports = router
