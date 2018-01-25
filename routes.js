6291c67f8895b3285d3eb206b2185a6dc9b817a8
kahu2018 <fake@fake.com>
2018-01-26 12:02:54 +1300

    Create attack selection page

--- routes.js
+++ routes.js
@@ -6,35 +6,50 @@ var db = require('knex')(config)
 var bodyParser = require('body-parser')
 
 router.get('/', function (req, res) {
-
     var db= req.app.get('db')
     db('monsters')
     .then(monsters => {
         db('cities')
             .then(cities => {
                 res.render('home', {monsters, cities})
-
             })
     })
     .catch(err => res.send("RAWR, an error!", err))
+})
+
+router.get('/attack', (req, res) => {
+    var db = req.app.get('db')
+
+    db('monsters')
+    .then(monsters => {
+        db('cities')
+        .then (cities => {
+            res.render('attack', { monsters, cities })
+        })
+    })
 })
 
 
 router.get('/profiles/:id', (req, res) => {
   var db = req.app.get('db')
   var id = req.params.id
+  
   db('monsters')
     .select()
-    .join('cities', 'monsters.id', '=', 'cities.monster_id')
-    .where('monsters.id', 'id')
-    .first()
-    .then((monster) => {
-      // monster = {monster:"hello", img:"monster-moo.png", description:"scary"}
-      res.render('profiles', monster)
-    })
-    .catch((err) => {
-      res.send("Monsters have taken control of the server")
-    })
+    // .join('cities', 'monsters.id', '=', 'cities.destroyed')
+    // .where('monsters.id', id)
+    // console.log('this is the ID:', monsters)
+
+    // .first()
+    // .then(data => {
+    //   // monster = {monster:"hello", img:"monster-moo.png", description:"scary"}
+      res.render('profiles')
+    // })
+    // .catch((err) => {
+    //   res.send("Monsters have taken control of the server")
+    // })
 })
+
+
 
 module.exports = router
