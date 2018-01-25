b70400ca15de66ddc080c6aa47df551096435286
kahu_2018 <fake.@fake.com>
2018-01-25 18:24:06 +1300

    udate to routes

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
 //const db = require('../db')
+//var environment = process.env.NODE_ENV || 'development'
+//var config = require('./knexfile')[environment]
+//var db = require('knex')(config)
 const router = express.Router()
 
 router.get('/', function (req, res) {
